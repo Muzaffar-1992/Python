86115aa3e6c58e2196bb8363bb341ecccdce53e5
Muzaffar-1992 <abdulazizovmuzaffar1992@gmail.com>
2025-10-23 21:21:43 +0500

    Create lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,42 @@
+# 1. Given a side of square. Find its perimeter and area.
+
+# Kvadrat tomoni=a
+
+a = 4
+P = 4 * a
+S = a * a
+print("Kvadrat perimetr:", P)
+print("kvadrat yuzi:", S)
+
+# 2. Given diameter of circle. Find its length.
+
+# Aylana uzunligini topish C=π*d
+# C- aylana uzunligi
+# d-diametr
+
+d = 10
+π = 3,14
+C = 3,14 * d
+print("Aylana uzunligi:", C)
+
+# 3. Given two numbers a and b. Find their mean.
+
+a=10
+b=12
+A=(a+b)/2
+
+print("o`rta arifmetik:", A)
+
+# 4. Given two numbers a and b. Find their sum, product and square of each number.
+
+a=260
+b=330
+A=a+b
+B=a*b
+S=a*a
+D=b*b
+
+print("Yigindisi", A)
+print("Ko`paytmasi:", B)
+print("A sonini kvadrati:", S)
+print("B sonini kvadrati:", D)
